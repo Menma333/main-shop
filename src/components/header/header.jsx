71e9e30ddd8dcf1eb2d7761345a60f8f9c57809e
Shvager Artur <menma0n@gmail.com>
2023-05-20 23:34:03 +0200

    make nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,23 @@
 import './header.css'
+import logoImg from './../../img/fashion-images/icons/logo.svg'
 
 function Header () {
   return <header className="header">
     <div className="container">
       <div className="header__row">
         <div className="header__logo">
-          LOGO
+          <img src={logoImg} alt="Logo" />
+          <span>Fashion</span>
         </div>
-        <div className="header__nav"></div>
+        <nav className="header__nav">
+          <ul>
+            <li><a href="#!">CATALOGUE</a></li>
+            <li><a href="#!">FASHION</a></li>
+            <li><a href="#!">FAVOURITE</a></li>
+            <li><a href="#!">LIFESTYLE</a></li>
+            <li><a href="#!">SING UP</a></li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
