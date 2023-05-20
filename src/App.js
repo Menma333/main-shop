04b3225262b5fdfae38a99af5b7fc73ec3017b9f
Shvager Artur <menma0n@gmail.com>
2023-05-20 23:43:55 +0200

    Promo section layout started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Header from './components/header/header'
 
 function App() {
-  const project = 'Fashion';
 
 
   return (
     <div className="App">
-      <h1>{project}</h1>
       <Header />
     </div>
   );
