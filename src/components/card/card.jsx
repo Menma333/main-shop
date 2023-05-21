22d2e8548467a34ffe0d535c0067505d3a2ec851
Shvager Artur <menma0n@gmail.com>
2023-05-21 13:06:34 +0200

    finish

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,12 +2,15 @@ import styles from './Card.module.css'
 import cat01Img from './../../img/fashion-images/categories/cat-01.jpg'
 import arrowImg from './../../img/fashion-images/icons/arrow.svg'
 
-const Card = () => {
+const Card = (props) => {
+  
   return (<div className={styles.card}>
-    <img className={styles.card__img} src={cat01Img} alt="Cate01" />
+    <a href="#!" className={styles.card__link}></a>
+    <img className={styles.card__img} src={props.img} alt="Cate01" />
+
     <div className={styles.card__body}>
     <div className={styles.card__text}>
-    <div className={styles.card__title}>Hoodies & Sweetshirt</div>
+    <div className={styles.card__title}>{props.title}</div>
     <div className={styles.card__muted}>Explore Now!</div>
       </div>
       <div className={styles.card__icon}>
--- src/components/new arrivals/arrivals.jsx
+++ src/components/new arrivals/arrivals.jsx
@@ -1,6 +1,10 @@
 import Card from '../card/card';
 import './arrivals.css'
 
+import cat01Img from './../../img/fashion-images/categories/cat-01.jpg'
+import cat02Img from './../../img/fashion-images/categories/cat-02.jpg'
+import cat03Img from './../../img/fashion-images/categories/cat-03.jpg'
+
 const Arrivals = () => {
   return (<section className='arrivals'>
     <div className="container">
@@ -8,9 +12,9 @@ const Arrivals = () => {
       <h2 className="title-2">NEW ARRIVALS</h2>
      </div>
      <div className="arrivals__cards">
-      <Card />
-      <Card />
-      <Card />
+      <Card title="Hoodies & Sweetshirt" img={cat01Img}/>
+      <Card title="Coats & Parkas" img={cat02Img}/>
+      <Card title="Tees & T-Shirt" img={cat03Img}/>
      </div>
     </div>
   </section>);
